f250bc6393505cb18e3aa39b9afe01e5968f508a
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-03-17 15:39:54 -0400

    added functions for clear and equals including the buttons

--- js/script.js
+++ js/script.js
@@ -2,47 +2,104 @@ $(document).ready(function(){
 
 	document.onkeyup = keyCheck;
 
-	function keyCheck(){111
+	$('.operator, .number').click(function(){
+		if ($(this).hasClass('equals')){
+			doIt();
+		}else if($(this).hasClass('clear')){
+			$('.screen').val('');
+		}else{
+			var currScreenVal = $('.screen').val();
+		$('.screen').val(currScreenVal + $(this).val());
+		}
+	})
+
+	function keyCheck(){
 		var keyID = event.keyCode;
 		console.log(keyID);
+		var currScreenVal = $('.screen').val();
 		switch(keyID){
-			case 49:
-				$('.screen').val('1');
-				break;
-			case 50:
-				$('.screen').val('2');
-				break;
-			case 51:
-				$('.screen').val('3');
-				break;
-			case 52:
-				$('.screen').val('4');
-				break;
-			case 53:
-				$('.screen').val('5');
-				break;
-			case 54:
-				$('.screen').val('6');
-				break;
-			case 55:
-				$('.screen').val('7');
-				break;
-			case 56:
-				$('.screen').val('8');
-				break;
-			case 57:
-				$('.screen').val('9');
+			case 27:
+				clearScreen();
 				break;
 			case 58:
-				$('.screen').val('0');
+				$('.screen').val(currScreenVal + 0);
+				break;
+			case 49:
+				$('.screen').val(currScreenVal + 1);
+				break;
+			case 50:
+				$('.screen').val(currScreenVal + 2);
+				break;
+			case 51:
+				$('.screen').val(currScreenVal + 3);
+				break;
+			case 52:
+				$('.screen').val(currScreenVal + 4);
+				break;
+			case 53:
+				$('.screen').val(currScreenVal + 5);
+				break;
+			case 54:
+				$('.screen').val(currScreenVal + 6);
+				break;
+			case 55:
+				$('.screen').val(currScreenVal + 7);
+				break;
+			case 56:
+				$('.screen').val(currScreenVal + 8);
+				break;
+			case 57:
+				$('.screen').val(currScreenVal + 9);
 				break;
 			case 187:
-				$('.screen').val('=');
+				// //equal sign
+				// $('.screen').val('=');
+				doIt();
 				break;
 			case 191:
-				$('.screen').val('/');
+				$('.screen').val(currScreenVal + '/');
 				break;
-			
+			case 88:
+				$('.screen').val(currScreenVal +'*');
+				break;
+			case 189:
+				$('.screen').val(currScreenVal + '-');
+				break;
+			case 107:
+				$('.screen').val(currScreenVal + '+');
+				break;
+			case 13:
+				// enter sign
+				doIt();
+				break;
+			case 190:
+				$('.screen').val(currScreenVal + '.');
 		}
 	}
-});
+})
+
+// function clearScreen(){
+// 	$('.screen').val("");
+// }
+
+
+// function insertVal(val){
+// 	var currScreenVal = $('.screen').val();
+// 	$('.screen').val(currScreenVal + val);
+// }
+
+function doIt(){
+	try{
+		var total = eval($('.screen').val());
+	}catch(e){
+		console.log(e);
+		if(e instanceof SyntaxError){
+			alert("Try again.");
+			return;
+		}else if(e instanceof ReferenceError){
+			alert("Dang");
+			return;
+		}
+	}
+	$('.screen').val(total);
+}
